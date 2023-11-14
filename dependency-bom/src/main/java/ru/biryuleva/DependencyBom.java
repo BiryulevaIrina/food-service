f721d08864acd50402866ccc81adce14568b50ff
Ирина <birka1976@yandex.ru>
2023-11-14 12:58:22 +0300

    feat: создание модулей, внедрение зависимостей

---
+++ dependency-bom/src/main/java/ru/biryuleva/DependencyBom.java
@@ -0,0 +1,11 @@
+package ru.biryuleva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyBom {
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyBom.class);
+    }
+}
---
+++ notification-service/src/main/java/ru/biryuleva/NotificationService.java
@@ -0,0 +1,7 @@
+package ru.biryuleva;
+
+public class NotificationService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
